79fae824d80d82ef6f67b928aa386e0001ced6dc
Michael <mvasilyev04@gmail.com>
2024-12-30 22:13:55 +0300

    added file input

--- task_logger/main.py
+++ task_logger/main.py
@@ -1,12 +1,26 @@
 from ParserOfElements import ParserOfElements
+import openpyxl
+import os
 
 def main():
     url = input("Введите задачу с LeetCode: ")
+    file = input("Введите путь до файла: ")
+
     row = []
     poe = ParserOfElements()
     html = poe.load_html(url)
     row = poe.parse(html, row)
-    print(row)
+    row.append(url)
+    row.append('Solved')
+    wb = 0
+    if os.path.exists(file):
+        wb = openpyxl.load_workbook(file)
+    else:
+        wb = openpyxl.Workbook()
+    ws = wb.active
+    ws.title = "LeetCode"
+    ws.append(row)
+    wb.save(file)
 
 
 if __name__ == "__main__":
